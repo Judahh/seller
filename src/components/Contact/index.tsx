47dcd0a450b61ce65e5aa6df59a1595e3228361a
Judah <judahholanda7@gmail.com>
2022-11-07 10:40:17 -0300

    add contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -3,51 +3,46 @@ import {
   FlexContainer,
   SimpleContentWrapper,
 } from 'minimal-components-react/dist/components/Content';
-import React from 'react';
+import React, { useContext } from 'react';
 // import { Link } from 'minimal-components-react/dist/components/Text';
-// { useContext } from "react";
-// import { LanguageContext } from "../../lang/context";
 // import { HiOutlineMail } from "react-icons/hi";
-// import { ImWhatsapp } from "react-icons/im";
+import { ImWhatsapp } from 'react-icons/im';
 // import { SiInstagram } from "react-icons/si";
-// import { default as socialMedia } from "./socialMedia.json";
+import { default as socialMedia } from './socialMedia.json';
 import { withTheme } from 'styled-components';
+import IconModel from '../../models/iconModel';
+import { LanguageContext } from '../../contexts/language/context';
+import { Link } from 'minimal-components-react/dist/components/Text';
 
 const Contact = () => {
-  // const { contact } = useContext(LanguageContext);
+  const { contact } = useContext(LanguageContext);
 
-  // const icons = {
-  //   "WhatsApp": {
-  //     icon: <ImWhatsapp size={18} />
-  //   },
-  //   "Instagram": {
-  //     icon: <SiInstagram size={18} />
-  //   },
-  //   "Email": {
-  //     icon: <HiOutlineMail size={18} />
-  //   }
-  // };
+  const icons = {
+    WhatsApp: {
+      icon: <ImWhatsapp size={50} color={'#0ACF83'} />,
+    },
+  };
 
-  // const iconsList: Array<IconModel> = [];
+  const iconsList: Array<IconModel> = [];
 
-  // for (const key in icons) {
-  //   if (icons.hasOwnProperty(key)) {
-  //     const icon = icons[key];
-  //     const href = socialMedia[key];
-  //     const text = contact.socialMedia[key];
-  //     iconsList.push({
-  //       icon: icon.icon,
-  //       href: href.href + text.text,
-  //       alt: key
-  //     })
-  //   }
-  // }
+  for (const key in icons) {
+    if (icons.hasOwnProperty(key)) {
+      const icon = icons[key];
+      const href = socialMedia[key];
+      const text = contact.socialMedia[key];
+      iconsList.push({
+        icon: icon.icon,
+        href: href.href + text.text,
+        alt: key,
+      });
+    }
+  }
 
   return (
     <FlexContainer id="contact">
       <SimpleContentWrapper>
         <ItemList>
-          {/* {iconsList.map((e, index) => {
+          {iconsList.map((e, index) => {
             return (
               <Link
                 icon
@@ -60,7 +55,7 @@ const Contact = () => {
                 <a href={e.href}>{e.icon}</a>
               </Link>
             );
-          })} */}
+          })}
         </ItemList>
       </SimpleContentWrapper>
     </FlexContainer>
